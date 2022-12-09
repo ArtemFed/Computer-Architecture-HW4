348add9aee13fde57614eb7acaa6a1ffb5ba12f3
ArtemFed <57373162+ArtemFed@users.noreply.github.com>
2022-12-09 17:03:16 +0300

    v1.0.1

--- main.cpp
+++ main.cpp
@@ -12,25 +12,15 @@ using namespace std;
 
 #define SECTIONS_NUM 3
 #define BUYERS_NUM 3
-#define COUNT 3
+#define COUNT 1
 
 string ANSI_RESET = "\033[0m";
-string ANSI_BLACK = "\033[30m";
 string ANSI_RED = "\033[31m";
 string ANSI_GREEN = "\033[32m";
 string ANSI_YELLOW = "\033[33m";
 string ANSI_BLUE = "\033[34m";
 string ANSI_PURPLE = "\033[35m";
 string ANSI_CYAN = "\033[36m";
-
-string func(string str, int id) {
-    if (id == 0) {
-        return ANSI_YELLOW + str + ANSI_RESET;
-    } else if (id == 1) {
-        return ANSI_GREEN + str + ANSI_RESET;
-    }
-    return ANSI_CYAN + str + ANSI_RESET;
-}
 
 /**
  * Класс продавца
@@ -40,6 +30,16 @@ public:
     int id = 0;
 
     Seller() {
+    }
+
+    // Раскрасить вывод Seller
+    static string getColor(const string &str, int id) {
+        if (id == 0) {
+            return ANSI_YELLOW + str + ANSI_RESET;
+        } else if (id == 1) {
+            return ANSI_GREEN + str + ANSI_RESET;
+        }
+        return ANSI_CYAN + str + ANSI_RESET;
     }
 };
 
@@ -60,7 +60,7 @@ vector<Buyer *> buf(SECTIONS_NUM);
 
 bool flag = true;
 
-unsigned int seed = 102;// инициализатор генератора случайных чисел
+unsigned int seed = 101;// инициализатор генератора случайных чисел
 
 pthread_mutex_t mutex;// мьютекс для условных переменных
 
@@ -76,18 +76,18 @@ void *SellerFunc(void *param) {
             continue;
         }
 
-        cout << func("\nSeller: " + to_string(seller.id) + " IS serving for Buyer: " + to_string(buf[seller.id]->id) + " \t\tclock: " + to_string((clock())), seller.id);
+        cout << Seller::getColor("\nSeller: " + to_string(seller.id) + " IS serving for Buyer: " + to_string(buf[seller.id]->id) + " \t\tclock: " + to_string((clock())), seller.id);
 
         sleep(2);
 
-        cout << func("\nSeller: " + to_string(seller.id) + " ENDED serving for Buyer: " + to_string(buf[seller.id]->id) + " \t\tclock: " + to_string((clock())), seller.id);
+        cout << Seller::getColor("\nSeller: " + to_string(seller.id) + " ENDED serving for Buyer: " + to_string(buf[seller.id]->id) + " \t\tclock: " + to_string((clock())), seller.id);
 
         buf[seller.id] = nullptr;
 
         // Разбудить потоки-читатели после обновления элемента буфера
-//        pthread_cond_broadcast(&section_empty);
+        //        pthread_cond_broadcast(&section_empty);
     }
-    cout << func("\nSeller: " + to_string(seller.id) + " ended successfully... " + " \t\tclock: " + to_string((clock())), seller.id);
+    cout << Seller::getColor("\nSeller: " + to_string(seller.id) + " ended successfully... " + " \t\tclock: " + to_string((clock())), seller.id);
     return nullptr;
 }
 
@@ -97,7 +97,7 @@ void *BuyerFunc(void *param) {
     while (!buyer->plan.empty()) {
         // Извлечь элемент из буфера
 
-        cout << func("\nBuyer: " + to_string(buyer->id) + " moved in line to the Seller: " + to_string(buyer->plan.front()) + " \tclock: " + to_string(clock()), buyer->plan.front());
+        cout << Seller::getColor("\nBuyer: " + to_string(buyer->id) + " moved in line to the Seller: " + to_string(buyer->plan.front()) + " \tclock: " + to_string(clock()), buyer->plan.front());
 
         sleep(1);
 
@@ -112,7 +112,7 @@ void *BuyerFunc(void *param) {
 
         // Заснуть, если нужный сектор в магазине занят
         while (buf[buyer->plan.front()] != nullptr) {
-//            pthread_cond_wait(&section_empty, &mutex);
+            //            pthread_cond_wait(&section_empty, &mutex);
         }
 
         buyer->plan.pop();
@@ -125,36 +125,46 @@ void *BuyerFunc(void *param) {
 }
 
 int main() {
-    system("color 00");
-    cout << ANSI_CYAN << "HELLO" << ANSI_RESET;
+    /*
+     * Условие:
+     * 10. Задача о магазине - 1.
+     * В магазине работают три отдела, каждый отдел обслуживает один продавец.
+     * Покупатель, зайдя в магазин, делает покупки в одном или нескольких произвольных отделах,
+     * обходя их в произвольном порядке.
+     * Если в выбранном отделе продавец не свободен,
+     * покупатель становится в очередь и засыпает, пока продавец не освободится.
+     * Создать многопоточное приложение, моделирующее рабочий день магазина.
+     */
 
+    // Список продавцов
     std::vector<Seller> sellers(SECTIONS_NUM);
     std::vector<pthread_t> threads_sellers(sellers.size());
 
+    // Список покупателей
     std::vector<Buyer> buyers(BUYERS_NUM);
     std::vector<pthread_t> threads_buyers(buyers.size());
 
     srand(seed);
-    int i;
     // Инициализация мутексов и семафоров
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&section_empty, NULL);
 
-    std::cout << "\nThe store is opening!\n\n";
+    system("color 00");
+    std::cout << ANSI_PURPLE + "\nThe store is opening!\n\n" + ANSI_RESET;
 
     // Запуск продовцов (по одному в каждом отделе)
-    for (i = 0; i < sellers.size(); i++) {
+    for (int i = 0; i < sellers.size(); i++) {
         // Зададим id для продавца
         sellers[i].id = i;
         // Запускаем поток
         pthread_create(&threads_sellers[i], nullptr,
                        SellerFunc,
                        &sellers[i]);
-        cout << func("\nSeller: " + to_string(sellers[i].id) + " came to work", i);
+        cout << getColor("\nSeller: " + to_string(sellers[i].id) + " came to work", i);
     }
 
     // Запуск покупателей
-    for (i = 0; i < buyers.size(); ++i) {
+    for (int i = 0; i < buyers.size(); ++i) {
         buyers[i].id = i;
         // int num = rand() % 5;
         // Создаём случайный план для покупателя
@@ -187,6 +197,6 @@ int main() {
         pthread_join(threads_seller, nullptr);
     }
 
-    std::cout << "\n\nThe store is closed!";
+    std::cout << ANSI_PURPLE + "\n\nThe store is closed!" + ANSI_RESET;
     return 0;
 }
